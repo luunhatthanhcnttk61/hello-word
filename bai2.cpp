20681b943bba6261e6246eb4a2adcf2ff3021009
Chau Que Nhon <82493653+cqnsinger@users.noreply.github.com>
2021-11-23 11:17:22 +0700

    Update bai2.cpp

--- bai2.cpp
+++ bai2.cpp
@@ -1,27 +1,114 @@
-//bai2: nhap so 3 chu so , xuat ra hang tram , hang chuc , hang dvi
-
 #include<iostream>
 using namespace std;
-int kiemTraSoAm(int n)
-{
-	if (n > 0) return n;
-	else return n * (-1);
+//Cac thuat toan sap xep
+void swap(int &a, int &b){
+	int tmp = a;
+	a=b;
+	b=tmp;
 }
-int main()
-{
-	
-	
-	int a,b;
-	cout << " nhap so 3 chu so = ";
-	cin >> a;
-	b = kiemTraSoAm(a);							// dung ham : neu la so am thi chuyen sang so duong
-	while ((b < 100) || (b > 999))				// neu so khong phai so  3 chu so thi nhap lai
-	{
-		cout << " yeu cau nhap lai ";
-		cin >> b;
+void BubbleSort(int *a, int n){
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<n-i-1; j++){
+			if(a[j] > a[j+1])
+				swap(a[j], a[j+1]);
+		}
 	}
-	cout << " hang tram: " << b / 100;
-	cout << " hang chuc: " << (b / 10) % 10;
-	cout << " hang don vi " << b % 10;
-	return 0;
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
 }
+
+void SelectionSort(int *a, int n){
+	for(int i=0; i<n-1; i++){
+		//tìm vi tri phan tu co gia tri nho nhat
+		int min=a[i], pMin=i;
+		for(int j=i+1;j<n;j++){
+			if(min > a[j]){
+				min=a[j];
+				pMin=j;
+			}
+		}
+		if(i!=pMin){
+			swap(a[i], a[pMin]);
+		}
+	}
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
+}
+
+void InsertionSort(int *a, int n){
+	for (int i=1; i<n; i++){
+		int x=a[i];
+		int j=i-1;
+		while(j>=0&&a[j]>x){
+			a[j+i]=a[j];
+			j--;
+		}
+		a[j+1]=x;
+	}
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
+}
+void QuickSort(int *a, int left, int right){
+	int x=a[(left + right)/2];
+	int i=left, j=right;
+	do{
+		while(a[i]<x)
+			i++;
+		while(a[j]>x)
+			j++;
+		if(i<=j){
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}while(i<j);
+	if(left<j){
+		QuickSort(a, left, j);
+	}
+	if(right>i){
+		QuickSort(a, i, right);
+	}
+}
+
+//Cac thuat toan tim kiem
+int linearSearch(int *a, int n, int key){
+	for(int i=0; i<n; i++){
+		if(a[i]==key)
+			return i;
+	}
+	return -1;
+}
+
+int binarySearch(int *a, int n, int key){
+	int left=0, right=n-1, mid;
+	while(left<=right){
+		mid = (left+right)/2;
+		if(a[mid]==key)
+			return mid;
+		if(a[mid]<key)
+			right=mid+1;
+		else
+			right=mid-1;
+	}
+	return -1;
+}
+int main(){
+	int *a=new int;
+	int n;
+	cout<<"Nhap so phan tu: ";
+	cin>>n;
+	for(int i=0; i<n; i++){
+		cout<<"Nhap a["<<i<<"]: ";
+		cin>>a[i];
+	}
+	cout<<"Day da nhap: ";
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}	
+	cout<<"\nSap xep: ";
+	BubbleSort(a,n);
+	delete a;
+}		
