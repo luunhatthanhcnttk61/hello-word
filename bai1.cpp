365b6bc8c569335b231885efd860eac360f66248
Chau Que Nhon <82493653+cqnsinger@users.noreply.github.com>
2021-11-23 11:16:55 +0700

    Update bai1.cpp

--- bai1.cpp
+++ bai1.cpp
@@ -1,45 +1,130 @@
 //bai1: nhap xuat thong tin sinh vien
 
 #include<iostream>
-#include<string>
-#include<iomanip>
+#include<iostream>
 using namespace std;
-int main()
-{
-	
-
-	string hoten, mssv, lop, gioitinh, sdt;
-	cout << "ho ten: ";
-	getline(cin, hoten);
-	cout << "mssv: ";
-	cin >> mssv;
-	cout << "lop: ";
-	cin >> lop;
-	cout << "gioitinh: ";
-	cin >> gioitinh;
-	cout << "sdt: ";
-	cin >> sdt;
-	cout << endl;
-
-	cout << "*****THANH VIEN LOP " << lop << "*****" << endl << endl;
-	cout << "Ho ten: " << hoten << endl;
-	cout << "Ma so sinh vien: " << mssv << endl;
-	cout << "Lop: " << lop << endl << endl;
-	cout << "So dien thoai: " << sdt << setw(15) << "Gioi tinh: " << gioitinh << endl;
-	cout << "*********************************************";
-	//djtme
-	return 0;
+//Cac thuat toan sap xep
+void swap(int &a, int &b){
+	int tmp = a;
+	a=b;
+	b=tmp;
+}
+void BubbleSort(int a[], int n){
+	for(int i=0; i<n-1; i++){
+		for(int j=0; j<n-i-1; j++){
+			if(a[j] > a[j+1])
+				swap(a[j], a[j+1]);
+		}
+	}
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
 }
 
-	
-
-	
-
-
-	
-
-	
-	//bai8: 
-	return 0;
+void SelectionSort(int a[], int n){
+	for(int i=0; i<n-1; i++){
+		//tìm vi tri phan tu co gia tri nho nhat
+		int min=a[i], pMin=i;
+		for(int j=i+1;j<n;j++){
+			if(min > a[j]){
+				min=a[j];
+				pMin=j;
+			}
+		}
+		if(i!=pMin){
+			swap(a[i], a[pMin]);
+		}
+	}
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
 }
 
+void InsertionSort(int a[], int n){
+	for (int i=1; i<n; i++){
+		int x=a[i];
+		int j=i-1;
+		while(j>=0&&a[j]>x){
+			a[j+i]=a[j];
+			j--;
+		}
+		a[j+1]=x;
+	}
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}
+}
+void QuickSort(int a[], int left, int right){
+	int x=a[(left + right)/2];
+	int i=left, j=right;
+	do{
+		while(a[i]<x)
+			i++;
+		while(a[j]>x)
+			j++;
+		if(i<=j){
+			swap(a[i], a[j]);
+			i++;
+			j--;
+		}
+	}while(i<j);
+	if(left<j){
+		QuickSort(a, left, j);
+	}
+	if(right>i){
+		QuickSort(a, i, right);
+	}
+}
+
+//Cac thuat toan tim kiem
+int linearSearch(int a[], int n, int key){ 	7 9 4 2 15	7 4 2 9 15
+	for(int i=0; i<n; i++){
+		if(a[i]==key)
+			return i;
+	}
+	return -1;
+}
+
+int binarySearch(int a[], int n, int key){
+	int left=0, right=n-1, mid;
+	while(left<=right){
+		mid = (left+right)/2;
+		if(a[mid]==key)
+			return mid;
+		if(a[mid]<key)
+			return=mid+1;
+		else
+			right=mid-1;
+	}
+	return -1;
+}
+int main(){
+	int n;
+	int a[100];
+	cout<<"Nhap so phan tu: ";
+	cin>>n;
+	for(int i=0; i<n; i++){
+		cout<<"Nhap a["<<i<<"]: ";
+		cin>>a[i];
+	}
+	cout<<"Day da nhap: ";
+	for(int i=0; i<n; i++){
+		cout<<a[i]<<"\t";
+	}	
+	cout<<"\nSap xep: ";
+	BubbleSort(a,n);
+	
+}			
+//Do phuc tap cua thuat toan sap xep
+/*
+	QuickSort		O(nlogn)
+	BubbleSort		O(n^2)
+	SelectionSort	O(n^2)
+	InsertionSort	O(n^2)
+*/				
+//Do phuc tap cua thuat toan tim kiem
+/*
+	BinarySearch		O(nlogn)
+	LinearSearch		O(n^2)
+*/				
+				
